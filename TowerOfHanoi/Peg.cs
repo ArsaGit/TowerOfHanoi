cb839fe2d0e771e0851a6c07a32fc332394c2ea7
l <l>
2021-10-17 16:24:15 +0500

    recursive alg is ready

---
+++ TowerOfHanoi/Peg.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfHanoi
+{
+	public class Peg
+	{
+		public List<int> Rings { get; set; }
+
+		public Peg(int size, bool isFirst = false)
+		{
+			Rings = new List<int>(size);
+			if (isFirst) FillPeg(Rings);
+		}
+
+		private void FillPeg(List<int> rings)
+		{
+			for(int i = rings.Capacity; i > 0; i--)
+			{
+				rings.Add(i);
+			}
+		}
+
+		public int Remove()
+		{
+			int ring = Rings[Rings.Count - 1];
+			Rings.RemoveAt(Rings.Count - 1);
+			return ring;
+		}
+
+		public void Add(int ring)
+		{
+			Rings.Add(ring);
+		}
+
+		public void Print()
+		{
+			foreach (var e in Rings)
+				Console.WriteLine(e);
+		}
+	}
+}
---
+++ TowerOfHanoi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TowerOfHanoi
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RunRecursive();
+		}
+
+		static void RunRecursive()
+		{
+			const int N = 8;
+
+			var originalPeg = new Peg(N, true);
+			var additionalPeg = new Peg(N);
+			var finalPeg = new Peg(N);
+
+			RecutsiveHanoiTower(N, originalPeg, finalPeg, additionalPeg);
+
+			finalPeg.Print();
+		}
+
+		static void RecutsiveHanoiTower(int quantity, Peg from, Peg to, Peg buf_peg)
+		{
+			if (quantity != 0)
+			{
+				RecutsiveHanoiTower(quantity - 1, from, buf_peg, to);
+
+				to.Add(from.Remove());
+
+				RecutsiveHanoiTower(quantity - 1, buf_peg, to, from);
+			}
+		}
+
+
+	}
+}
